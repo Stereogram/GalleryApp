eb4bf0e8281983f53f8befcb63d5fad3081a24f1
Rian Neufelt <RNeufelt@live.ca>
2017-10-03 00:48:06 -0700

    asdf

--- app/src/main/java/com/example/unrea/galleryapp/FilterActivity.java
+++ app/src/main/java/com/example/unrea/galleryapp/FilterActivity.java
@@ -9,5 +9,6 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+        //asdf
     }
 }
